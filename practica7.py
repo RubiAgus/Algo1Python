3046d170e2a85b53f2f7db28eb45098f28d19025
Rubi <agustin.n.rubinstein@gmail.com>
2024-06-04 23:17:58 -0300

    finished part 1 of practica7

--- practica7.py
+++ practica7.py
@@ -1,3 +1,4 @@
+import array 
 
 #para la función pertenece el loop while itera toda la lista.
 
@@ -7,8 +8,8 @@ def pertenece (x, list1):
       if i  == x:
          return(True)
     else: return(False)
-    
-    
+   
+   
       
 # print(pertenece(1,[2,3,4,5,1,10,11]))
 
@@ -61,12 +62,76 @@ def isPalindrome (word:str) -> bool: # type: ignore
 
 #acá iría el caso fortaleza de un a contraseña pero lo haré mas tarde por que es divertido
 
-def analizar_fortaleza(password:str):
-   
-   return()
+def tieneMayuscula(password: str) -> bool:
+   for i in password:
+      if i.isupper():
+         return(True)
+   # return(False)
+# print(tieneMayuscula("pito"))
+def tieneMinuscula(password:str)->bool:
+   for i in password:
+      if i.islower():
+         return(True)
+   # return(False)
 
-def bank_statment (lst:[(chr,int)]):
-   for i in lst:
-      if lst[i][1] == 'i':
-        return()
+def tieneCHRespecial(password: str)-> bool:
+   for i in password: 
+      if not i.isalnum():
+         return(True)
+   return(False)
+      
+# print(tieneCHRespecial("aksljfagAKSLF!!HSAN"))
+
+def tieneNumero(password: str)-> bool:
+   for i in password:
+      if i.isnumeric():
+         return(True)
+   # return()
+# print(tieneNumero("asfjgd2w"))
+def condicionesVerde (a): 
+      if len(a)>8 and tieneNumero(a) and tieneCHRespecial(a) and tieneMayuscula(a)  and tieneMinuscula(a):
+         return(True)
+      else: return(False)
+
+def analizar_fortaleza(password:str) -> str:
+   if len(password) < 5:
+      return("ROJO")
+   elif condicionesVerde(password):
+
+      return("VERDE")
+   else:
+      return("AMARILLO")
+# print(analizar_fortaleza(input(("ingrese su contraseña: "))))
    
+from typing import List, Tuple
+
+def bank_statement(lista: List[Tuple[chr, int]]) -> int:
+    saldo = 0
+    for (transaction,amount) in lista:
+        if transaction == 'I':
+            saldo += amount
+        elif transaction == 'R':
+            saldo -= amount
+    return saldo
+
+# transactions = [('I', 100), ('R', 50), ('I', 200), ('R', 150)]
+# print(bank_statement(transactions))  # 100
+vowels = ['a','e','i', 'o', 'u']
+def isvowel(ltr: str):
+   for i in vowels:
+      if ltr == i:
+         return(True)
+      else: return(False)
+
+# print(isvowel("a"))
+
+def alMenos3Vocales(frase:str):
+   counter= 0
+   for i in str.lower(frase):
+      if isvowel(i):
+         counter += 1
+   
+   if counter >= 3:
+      return(True)
+   else: return(False)
+# print(alMenos3Vocales("cAAA"))
